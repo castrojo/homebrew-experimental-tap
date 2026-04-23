ddbda8d86ff80aa2e53a85b15c2b71800eee78d7
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-04-23 04:06:06 +0000

    emdash-linux 0.4.50

--- Casks/emdash-linux.rb
+++ Casks/emdash-linux.rb
@@ -1,6 +1,6 @@
 cask "emdash-linux" do
-  version "0.4.47"
-  sha256 "c98b1f7a79e97dbba6bc5bc1ccb317e67181829b21b97d9585ab4b226e921a13"
+  version "0.4.50"
+  sha256 "a692d69be0ce0279c6bf0a489ffc3f0cf2375832587279ba812af1397b090584"
 
   url "https://github.com/generalaction/emdash/releases/download/v#{version}/emdash-x86_64.AppImage"
   name "Emdash"
